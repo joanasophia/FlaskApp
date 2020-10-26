893c3206ac68f65599918eb25ba3e049ffe0514a
joanasophia <jsreuss@gmail.com>
2020-10-26 09:13:27 +0100

    new app.py

--- fooApp/app.py
+++ fooApp/app.py
@@ -153,41 +153,6 @@ def error_not_found(error):
 def error_not_found_invalid(error):
   return render_template('error/not_found.html'), 404 
 
-@app.route('/string/')
-def return_string():
-  dump = dump_request_detail(request)
-  return 'Hello, world!'
-
-@app.route('/object/')
-def return_object():
-  dump = dump_request_detail(request)
-  headers = {'Content-Type': 'text/plain'}
-  return make_response(Response('Hello, world! \n' + dump, status=200,
-    headers=headers))
-
-@app.route('/tuple/<path:resource>')
-def return_tuple(resource):
-  dump = dump_request_detail(request)
-  return 'Hello, world! \n' + dump, 200, {'Content-Type':
-    'text/plain'}
-
-
-def dump_request_detail(request):
-  request_detail = """
-## Request INFO ##
-request.endpoint: {request.endpoint}
-request.method: {request.method}
-request.view_args: {request.view_args}
-request.args: {request.args}
-request.form: {request.form}
-request.user_agent: {request.user_agent}
-request.files: {request.files}
-request.is_xhr: {request.is_xhr}
-
-## request.headers ##
-{request.headers}
-  """.format(request=request).strip()
-  return request_detail
 
 if __name__ == '__main__':
     app.run()
